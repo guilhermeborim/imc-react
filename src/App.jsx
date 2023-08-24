0dd97f23d1c06ad8ba44053b0ea16379a1ef68d0
Guilherme <borimdev@gmail.com>
2023-08-24 15:51:05 -0300

    atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Formulario from "./components/formulario";
 
 function App() {
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import styles from './Formulario.module.css';
 
 const Formulario = () => {
